30deb907025e0deb7f5c5a8928988066212a9e47
yingyanwu <1179858356@qq.com>
2022-08-25 18:00:56 +0800

    添加西式数字格式的显示

--- app/src/main/java/com/yyw/copyingioscalculator/components/OutputView.kt
+++ app/src/main/java/com/yyw/copyingioscalculator/components/OutputView.kt
@@ -1,8 +1,6 @@
 package com.yyw.copyingioscalculator.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.MaterialTheme
@@ -12,8 +10,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.yyw.copyingioscalculator.exts.numFormatForUS
+import com.yyw.copyingioscalculator.ui.theme.CopyingIosCalculatorTheme
 
 
 /**
@@ -21,19 +22,19 @@ import androidx.compose.ui.unit.sp
  * 支持自适应文字大小
  * @param showNum 显示的输入内容或计算结果
  * @param textSize 设置文本字体大小
+ * @param modifier 从外部设置组件的大小
+ * @param changeFontSize 从外部设置字体大小
  */
 @Composable
-fun ResizeOutputView(showNum: String, textSize: Int, changeFontSize: () -> Unit) {
+fun ResizeOutputView(showNum: String, textSize: Int, modifier: Modifier = Modifier, changeFontSize: () -> Unit = {}) {
     var readyToDraw by remember { mutableStateOf(false) }
     Box(
-        Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(.3f),
+        modifier = modifier,
         contentAlignment = Alignment.BottomEnd
     ) {
         SelectionContainer {
             Text(
-                text = showNum,
+                text = showNum.numFormatForUS(),
                 fontSize = textSize.sp,
                 color = MaterialTheme.colors.secondary,
                 maxLines = 1,
@@ -55,5 +56,13 @@ fun ResizeOutputView(showNum: String, textSize: Int, changeFontSize: () -> Unit)
                 }
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreResizeOutputView() {
+    CopyingIosCalculatorTheme {
+        ResizeOutputView("1234", 100)
     }
 }
