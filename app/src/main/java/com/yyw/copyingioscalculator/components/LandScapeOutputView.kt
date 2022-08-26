cd50577edae262ade637dd768d705cef47bf344c
yingyanwu <1179858356@qq.com>
2022-08-26 18:49:26 +0800

    添加横屏适配

---
+++ app/src/main/java/com/yyw/copyingioscalculator/components/LandScapeOutputView.kt
@@ -0,0 +1,51 @@
+package com.yyw.copyingioscalculator.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yyw.copyingioscalculator.exts.numFormatForUS
+import com.yyw.copyingioscalculator.ui.theme.CopyingIosCalculatorTheme
+
+/**
+ * 上部输出框，显示结果
+ * 支持自适应文字大小
+ * @param showNum 显示的输入内容或计算结果
+ * @param modifier 从外部设置组件的大小
+ */
+@Composable
+fun LandScapeOutputView(showNum: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        SelectionContainer {
+            Text(
+                text = showNum.numFormatForUS(),
+                fontSize = 50.sp,
+                color = MaterialTheme.colors.secondary,
+                maxLines = 1,
+                softWrap = false,
+                overflow = TextOverflow.Visible,
+                modifier = Modifier/*.background(color = Color.Green)*/
+                    .padding(horizontal = 14.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreOutputView() {
+    CopyingIosCalculatorTheme {
+        LandScapeOutputView("1234")
+    }
+}
